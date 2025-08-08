77148633b2cbb767557117ef00a36a973f964657
johan-l-r <188136149+johan-l-r@users.noreply.github.com>
2025-08-08 09:24:38 -0500

    feat: add chess board pattern
    this helps users to see how far they are from the borders

--- main.py
+++ main.py
@@ -10,6 +10,21 @@ CELL_SIZE = WINDOW_SIZE / MAX_CELLS
 
 window = pg.display.set_mode((800, 800))
 
+def draw_grid():
+  current_cell_color = (0, 0, 0)
+
+  for row in range(MAX_CELLS):
+    for col in range(MAX_CELLS):
+      cell = pg.Rect(row * CELL_SIZE, col * CELL_SIZE, CELL_SIZE, CELL_SIZE)
+
+      # create chess board pattern
+      if (row + col) % 2 == 0:
+        current_cell_color = (20, 20, 20)
+      else:
+        current_cell_color = (0, 0, 0)
+
+      pg.draw.rect(window, current_cell_color, cell)
+
 while is_running:
   for event in pg.event.get():
     if event.type == pg.QUIT:
@@ -18,3 +33,6 @@ while is_running:
   # update game
 
   # draw game
+  draw_grid()
+
+  pg.display.flip()
