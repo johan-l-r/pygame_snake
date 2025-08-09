9181692c2c63d349e79d2199121e38cd8b78f84c
johan-l-r <188136149+johan-l-r@users.noreply.github.com>
2025-08-09 18:33:31 -0500

    feat: add snake movement
    now the player can move around

--- main.py
+++ main.py
@@ -8,7 +8,18 @@ WINDOW_SIZE = 800 # 800x800
 MAX_CELLS = 25
 CELL_SIZE = WINDOW_SIZE / MAX_CELLS
 
+# [0] = x [1] = y
+movement_direction = [1, 0]
+
+snake_x_pos = 12 * CELL_SIZE
+snake_y_pos = 12 * CELL_SIZE
+
+move_timer = 0
+move_time = 5
+
 window = pg.display.set_mode((800, 800))
+
+snake_head = pg.Rect(snake_x_pos, snake_y_pos, CELL_SIZE, CELL_SIZE)
 
 def draw_grid():
   current_cell_color = (0, 0, 0)
@@ -29,10 +40,27 @@ while is_running:
   for event in pg.event.get():
     if event.type == pg.QUIT:
       is_running = False
+    if event.type == pg.KEYDOWN:
+      if event.key == pg.K_w or event.key == pg.K_UP:
+        movement_direction = [0, -1]
+      if event.key == pg.K_s or event.key == pg.K_DOWN:
+        movement_direction = [0, 1]
+      if event.key == pg.K_a or event.key == pg.K_LEFT:
+        movement_direction = [-1, 0]
+      if event.key == pg.K_d or event.key == pg.K_RIGHT:
+        movement_direction = [1, 0]
 
   # update game
+  move_timer += 0.1
+
+  if move_timer >= move_time:
+    snake_head.x += int(movement_direction[0] * CELL_SIZE)
+    snake_head.y += int(movement_direction[1] * CELL_SIZE)
+
+    move_timer = 0
 
   # draw game
   draw_grid()
+  pg.draw.rect(window, (255, 255, 255), snake_head)
 
   pg.display.flip()
